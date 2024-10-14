144a55e12ba272edf3b28eadcb55a46c3b4ed16a
Yasuhiro WATANABE <info@nabeyasu.com>
2024-10-14 14:21:52 +0900

    Refactor start-server.sh to include PHP-FPM startup command

--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,8 @@
 # Apacheをフォアグラウンドで起動
 httpd -D FOREGROUND &
 
+# PHP-FPMをフォアグラウンドで起動
+php-fpm -D &
+
 # uWSGIを起動してFlaskアプリケーションを実行
 uwsgi --ini /var/www/html/arch-struct-analysis/arch-struct-analysis.ini
