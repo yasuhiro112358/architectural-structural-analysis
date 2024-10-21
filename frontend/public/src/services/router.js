902d0df4dc729f08fd653b315171aaa27298ac1f
Yasuhiro WATANABE <info@nabeyasu.com>
2024-10-21 21:11:23 +0900

    Refactor router.js to fix navigation bug

--- frontend/public/src/services/router.js
+++ frontend/public/src/services/router.js
@@ -1,6 +1,6 @@
 export const router = {
   navigateTo(path) {
-    window.history.pushState({}, path, window.location.origin + path);
+    window.history.pushState({}, '', window.location.origin + path);
   },
   getCurrentRoute() {
     return window.location.pathname;
