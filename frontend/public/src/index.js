0c89f77c167f06a78ff42f35ec14cfa99a390774
Yasuhiro WATANABE <info@nabeyasu.com>
2024-10-22 23:56:09 +0900

    Refactor link handling in index.js

--- frontend/public/src/index.js
+++ frontend/public/src/index.js
@@ -1,4 +1,4 @@
-import App  from './App.js';
+import App from './App.js';
 import { router } from './services/router.js';
 
 // Initial render
@@ -11,10 +11,16 @@ window.addEventListener('popstate', () => {
 
 document.body.addEventListener('click', (e) => {
   if (e.target.matches('a')) {
+    const target = e.target;
+    const href = target.getAttribute('href');
+    const isExternal = target.hostname !== window.location.hostname;
+
+    if (isExternal) {
+      return; // 外部リンクの場合はデフォルトの動作を許可
+    }
+
     e.preventDefault();
-    
-    const path = e.target.getAttribute('href');
-    router.navigateTo(path);
+    router.navigateTo(href);
     App();
   }
 });
